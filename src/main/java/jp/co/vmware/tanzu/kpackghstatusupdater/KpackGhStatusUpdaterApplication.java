e1879ae19bf5de6f1e2c0522e37b5bb4721c4dd5
machih <machih@vmware.com>
2022-11-26 14:58:05 +0900

    Initial commit

---
+++ src/main/java/jp/co/vmware/tanzu/kpackghstatusupdater/KpackGhStatusUpdaterApplication.java
@@ -0,0 +1,13 @@
+package jp.co.vmware.tanzu.kpackghstatusupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpackGhStatusUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpackGhStatusUpdaterApplication.class, args);
+    }
+
+}
